281bb1ff3f8a5d88f423851e3d08f6ce82f894cd
Rajeev <rajeev@i2vsys.com>
2021-12-17 21:25:02 +0530

    social media

--- src/components/routes/off.js
+++ src/components/routes/off.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Signup from '../auth/signup';
 import Login from '../auth/login';
 import {Switch,Redirect,Route} from 'react-router-dom';
-import TemplateHome from '../TemplateHome';
+
 
 const Off = ()=>{
 	return <div  className="wrapper">
